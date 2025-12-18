430af12d825d17f75b5ca864f6176211ec136f34
dev-sadeeq0023 <sadeeqln8@gmail.com>
2025-12-18 12:43:37 +0500

    apis added

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -34,7 +34,6 @@ async function getCurrentByProfile(req, res) {
   }
 }
 
-
 async function getSubscriptions(req, res) {
   try {
     // Check if user is CRM
@@ -55,6 +54,8 @@ async function getSubscriptions(req, res) {
 
     if (isCurrent === "true") {
       query.isCurrent = true;
+    }else if (isCurrent === "false") {
+      query.isCurrent = false;
     }
 
     const subscriptions = await Subscription.find(query)
