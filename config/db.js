8fe9862c8353aca6adf886b2a7365090e440832a
Kashif <kashif147@gmail.com>
2026-06-23 00:11:46 +0100

    bin/subscription-service, config/db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 exports.mongooseConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then((data) => {
-      console.log(`SuccessFully Connected to the MongoDB ===> "${data.connection.name}"`);
-    });
+    const data = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `SuccessFully Connected to the MongoDB ===> "${data.connection.name}"`,
+    );
+    return data;
   } catch (e) {
     console.error("Unable to connect to database:", e);
+    throw e;
   }
 };
